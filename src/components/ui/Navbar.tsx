ac8a9e69d516d7d309dac6b216969b216bba9ac7
lucasfedumenticastro <144843973+trueluuck@users.noreply.github.com>
2025-07-21 13:12:25 -0300

    Navbar.tsx

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -18,7 +18,7 @@ export default function Navbar() {
     <nav className="fixed w-full bg-white z-50 shadow">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
-          <Link href="/" className="text-blue-700 font-bold text-xl">Fedumenti</Link>
+          <Link href="/" className="text-blue-700 font-bold text-xl">FCGroup - FEDUMENTI GROUP</Link>
           <div className="hidden md:flex space-x-4">
             {navItems.map((item) => (
               <Link key={item.href} href={item.href} scroll={false} className="text-gray-700 hover:text-blue-700 transition">
